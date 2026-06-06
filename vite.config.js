1deac36e39f657a9663019f38b4ebfedfdd62601
vivek rawat <viveksingh9554360@gmail.com>
2026-06-06 11:36:21 +0530

    My Portfolio Build

---
+++ vite.config.js
@@ -0,0 +1,22 @@
+import { defineConfig } from "vite";
+import { resolve } from "path";
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        contact: resolve(__dirname, "contact.html"),
+        work: resolve(__dirname, "work.html"),
+      },
+    },
+    assetsInclude: [
+      "**/*.jpeg",
+      "**/*.jpg",
+      "**/*.png",
+      "**/*.svg",
+      "**/*.gif",
+    ],
+    copyPublicDir: true,
+  },
+});
